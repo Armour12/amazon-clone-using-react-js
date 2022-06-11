9a74b412752681876b4b4ee56f523113fdef7125
Armour12 <pateldhrna17ce@student.mes.ac.in>
2022-06-11 22:38:23 +0530

    added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAl5MEgeN3CX-lPECYFqp8CvbmGMWx5xqk",
+    authDomain: "clone-b34f9.firebaseapp.com",
+    projectId: "clone-b34f9",
+    storageBucket: "clone-b34f9.appspot.com",
+    messagingSenderId: "377332903839",
+    appId: "1:377332903839:web:1e9837d1eb302a0cc617aa",
+    measurementId: "G-DKGXN8B7KB"
+  };
