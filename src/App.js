16e88ec9759da68b988d3996c647f756efce1bd8
Armour12 <pateldhrna17ce@student.mes.ac.in>
2022-06-13 20:18:13 +0530

    checkout added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React from 'react';
 import './App.css';
 import Header from './Header';
-
-
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Checkout from './Checkout';
 function App() {
   return (
-    //BEM
-    <div className="app">
-    <Header />
-    {/*Home*/}
+    
+    
+    <Router>
+      <div className="app">
+      <Header />
+      <Routes>
+        <Route path="*" element={<Home/>}/>
+        <Route exact path="/checkout" element={<Checkout/>}/>
+      </Routes>
+    
     </div>
+  </Router>
+ 
   );
 }
 
